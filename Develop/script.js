3bcb640a9c59e0bc7e458ea0127a0ce67c10abe5
Jeremy Gieske <jeremy.gieske@gmail.com>
2021-12-12 00:26:11 -0600

    added character arrays and input logic for length and lowercase

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,43 @@
 // Assignment code here
 
+const lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const specialCharacters = ["!","$", "%", "?", "*", "@"]
+
+const length = () => {
+  let userInput = 0
+  while (userInput < 8 || userInput > 128 ) {
+  userInput = window.prompt("Enter a password length between 8 and 128 characters.")
+  }
+  return userInput
+}
+
+const lowerCase = () => {
+  let userInput = window.prompt("Yes/No - include lower case letters?").toLowerCase()
+  // userInput = userInput.toLowerCase()
+  if (userInput === "yes") {
+    return true
+  } else if 
+  (userInput === "no") { 
+    return false
+  } else { 
+    window.alert("Enter \"Yes\" or \"No\".")
+    lowerCase()
+  }
+}
+
+const generatePassword = () => {
+  
+  //get password length
+  const pwLength = length()
+  console.log(pwLength)
+
+  //include lower case?
+  const pwLowerCase = lowerCase()
+  console.log(pwLowerCase)
+
+  return "Here is your password"
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
