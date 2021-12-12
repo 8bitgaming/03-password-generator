c4ba0c916930690f2847cdc596190e61917151af
Jeremy Gieske <jeremy.gieske@gmail.com>
2021-12-12 01:26:58 -0600

    change to arrays and switch statement

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,8 @@
 const lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 const upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 const specialCharacters = ["!","$", "%", "?", "*", "@"]
+const numbersList = ["0", "1","2","3","4","5","6","7","8","9"]
+let approvedList = []
 
 const length = () => {
   let userInput = 0
@@ -14,15 +16,30 @@ const length = () => {
 
 const numbers = () => {
   let userInput = window.prompt("Yes/No - include numbers?").toLowerCase()
-  if (userInput === "yes" || userInput === "y" ) {
-    return true
-  } else if 
-  (userInput === "no" || userInput === "n") { 
-    return false
-  } else { 
-    window.alert("Enter \"Yes\" or \"No\".")
-    numbers()
+
+  switch (userInput) {
+    case "yes":
+    case "y":
+      approvedList = approvedList.concat(numbersList)
+      break;
+    case "no":
+    case "n":
+      break;
+    default:
+      window.alert("Enter \"Yes\" or \"No\".")
+      numbers()
   }
+
+  // if (userInput === "yes" || userInput === "y" ) {
+  //   approvedList = approvedList.concat(numbersList)
+  //   return
+  // } else if 
+  // (userInput === "no" || userInput === "n") { 
+  //   return
+  // } else { 
+  //   window.alert("Enter \"Yes\" or \"No\".")
+  //   numbers()
+  // }
 }
 
 const lowerCase = () => {
@@ -49,19 +66,23 @@ const generatePassword = () => {
   //get password length
   const pwLength = length()
   console.log(pwLength)
-
-  //include numbers
-  const pwNumbers = numbers()
+ 
+  //include numbers?
+  numbers()
+  console.log(approvedList)
 
   //include lower case?
   const pwLowerCase = lowerCase()
-  console.log(pwLowerCase)
-
-  let password = ''
-
   
+  //include special characters?
+
+  //check if approvedList array is empty - if yes alert and restart
+
+  //generate string based on input values
+  let password = ''
    for (let i = 0; i < pwLength; i++){
-    password = password + randomNum()
+    //select random from approvedList array
+    //how to make sure get at least one of each?
   }
   
   return password
